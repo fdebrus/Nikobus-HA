a69e4a3aa1840ffe537a89aa04192524f1f20c0b
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-07 13:00:27 +0100

    Update nikobus.py

--- custom_components/nikobus.py
+++ custom_components/nikobus.py
@@ -34,6 +34,63 @@ class Nikobus:
         # Return True to indicate that the integration was successfully set up
         return True
 
+def __int_to_hex(value, digits):
+    hex_str = format(value, 'X')
+    padded_hex = '0' * (8 - len(hex_str)) + hex_str
+    return padded_hex[-digits:]
+
+def __hex_to_int(value):
+    return int(value, 16)
+    
+def __int_to_dec(value, digits):
+    dec_str = str(value)
+    padded_dec = '0' * (8 - len(dec_str)) + dec_str
+    return padded_dec[-digits:]
+
+def __dec_to_int(value):
+    return int(value)
+
+def send_command(self, command, callback):
+    _LOGGER.debug('Nikobus.sendCommand() enter')
+    _LOGGER.debug('command = ' + command)
+    _LOGGER.debug('serial tx [' + command + '] (cr)')
+    
+    self.serial_port.write(command + '\r', write_callback)
+    
+    _LOGGER.debug('Nikobus.sendCommand() leave')
+
+def serial_port_on_data(self, data):
+    s = data.decode('utf-8')
+    i = 0
+    while i < len(s):
+        c = s[i]
+        if c == '\r':
+            _LOGGER.debug('serial rx [' + self.serial_rx_data + '] (cr)')
+            self.emit('command', self.serial_rx_data)
+            if self.get_answer_callback is not None:
+                # xxx$0512$1CB6020000FF000000FFC2B316
+                # xxx$0515$0EFFB6020030
+                # xxx$0516$0EFFB6020030
+                # xxx$0517$1CB60200FF0000000000D253EE
+                # xxx$0519$0EFFB6020030
+                line = self.serial_rx_data
+                j = line.rfind('$')
+                if j >= 5 and line[j - 5:j] == self.wait_command_ack:
+                    line = line[j:]
+                    # $1CB6020000FF000000FFC2B316
+                    # $0EFFB6020030
+                    _LOGGER.debug('answer received')
+                    if self.get_answer_timeout is not None:
+                        clearTimeout(self.get_answer_timeout)
+                        self.get_answer_timeout = None
+                    callback = self.get_answer_callback
+                    self.get_answer_callback = None
+                    callback(None, line)
+            self.serial_rx_data = ''
+        else:
+            self.serial_rx_data += c
+        i += 1
+
 class MyTCPSocketEntity(Entity):
     """Representation of a TCP socket entity."""
 
