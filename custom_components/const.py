7b819e1b161ed2839a4b0baee730612812c79a62
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-07 13:36:01 +0100

    Update const.py

--- custom_components/const.py
+++ custom_components/const.py
@@ -1,3 +1,23 @@
 """Constants."""
 
 DOMAIN = "nikobus"
+
+# List of all Thing Type UIDs
+BRIDGE_TYPE_PCLINK = f"{BINDING_ID}:pc-link"
+
+THING_TYPE_PUSH_BUTTON = f"{BINDING_ID}:push-button"
+THING_TYPE_SWITCH_MODULE = f"{BINDING_ID}:switch-module"
+THING_TYPE_DIMMER_MODULE = f"{BINDING_ID}:dimmer-module"
+THING_TYPE_ROLLERSHUTTER_MODULE = f"{BINDING_ID}:rollershutter-module"
+
+# List of all Channel ids
+CHANNEL_BUTTON = "button"
+CHANNEL_TRIGGER_FILTER = "trigger-filter"
+CHANNEL_TRIGGER_BUTTON = "trigger-button"
+CHANNEL_OUTPUT_PREFIX = "output-"
+
+# Configuration parameters
+CONFIG_REFRESH_INTERVAL = "refreshInterval"
+CONFIG_IMPACTED_MODULES = "impactedModules"
+CONFIG_ADDRESS = "address"
+CONFIG_PORT_NAME = "port"
