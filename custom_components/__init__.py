aa197510eddc4715469a3630b16af4ef611f2a0e
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-07 12:23:24 +0100

    Update __init__.py

--- custom_components/__init__.py
+++ custom_components/__init__.py
@@ -1,7 +1,7 @@
 """The Nikobus integration."""
 from homeassistant import config_entries, core
 from homeassistant.components import binary_sensor, light, switch, sensor, select
-from homeassistant.const import CONF_IP
+from homeassistant.const import CONF_IP, CONF_PORT
 
 from .const import DOMAIN
 from .coordinator import NikobusDataCoordinator
@@ -11,7 +11,7 @@ PLATFORMS = [binary_sensor.DOMAIN, light.DOMAIN, switch.DOMAIN, sensor.DOMAIN, c
 
 async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
     """Set up the Nikobus component."""
-    api = await Nikobus.create(async_get_clientsession(hass), entry.data[CONF_IP])
+    api = await Nikobus.create(async_get_clientsession(hass), entry.data[CONF_IP], entry.data[CONF_PORT])
     coordinator = NikobusDataCoordinator(hass, api)
     
     coordinator.data = await hass.async_add_executor_job(api.get_bridge, entry.data["bridge_id"])
