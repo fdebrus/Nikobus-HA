88e585b8c0dc7979ca8e872b379fa3d1d54ad125
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-07 16:16:45 +0100

    Create config_flow.py

---
+++ custom_components/nikobus/config_flow.py
@@ -0,0 +1,31 @@
+"""Config Flow."""
+from typing import Any, Optional
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_PORT
+import homeassistant.helpers.config_validation as cv
+
+from .const import DOMAIN
+from .nikobus import Nikobus, UnauthorizedException
+
+AUTH_SCHEMA = vol.Schema(
+    {vol.Required(CONF_HOST): cv.string, vol.Required(CONF_PORT): cv.string}
+)
+
+class NikobusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Nikobus config flow."""
+
+    data: Optional[dict[str, Any]]
+
+    async def async_step_user(self, user_input: Optional[dict[str, Any]] = None):
+        """Handle a flow initialized by the user."""
+        errors = {}
+        if user_input is not None:
+            self.data = user_input
+            return await self.async_step_pool()
+
+        return self.async_show_form(
+            step_id="user", data_schema=AUTH_SCHEMA, errors=errors
+        )
