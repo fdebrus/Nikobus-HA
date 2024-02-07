c47cbe4c5881f4d4458ab218d3339d3b6d184594
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-07 12:30:47 +0100

    Create nikobus.py

---
+++ custom_components/nikobus.py
@@ -0,0 +1,75 @@
+import asyncio
+import logging
+
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+
+from homeassistant.const import CONF_HOST, CONF_PORT
+from homeassistant.helpers.entity import Entity
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_HOST): cv.string,
+                vol.Required(CONF_PORT): cv.port,
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+class Nikobus:
+
+    async def async_setup(hass, config):
+        """Set up the TCP socket integration."""
+        conf = config[DOMAIN]
+        host = conf.get(CONF_HOST)
+        port = conf.get(CONF_PORT)
+
+        # Establish connection to TCP socket
+        reader, writer = await asyncio.open_connection(host, port)
+
+        # Example: send data to the socket
+        writer.write(b"Hello, TCP socket!\n")
+        await writer.drain()
+
+        # Example: read data from the socket
+        data = await reader.read(100)
+        _LOGGER.info("Received data from TCP socket: %s", data.decode())
+
+        # Optionally, you can create entities or services based on the data received
+
+        # Return True to indicate that the integration was successfully set up
+        return True
+
+class MyTCPSocketEntity(Entity):
+    """Representation of a TCP socket entity."""
+
+    def __init__(self):
+        """Initialize the entity."""
+        self._state = None
+
+    async def async_update(self):
+        """Update the entity."""
+        # This is where you can update the state of the entity based on data from the socket
+        # You can also perform additional operations here, such as sending commands to the socket
+        pass
+
+    @property
+    def name(self):
+        """Return the name of the entity."""
+        return "My TCP Socket Entity"
+
+    @property
+    def state(self):
+        """Return the state of the entity."""
+        return self._state
+
+    @property
+    def should_poll(self):
+        """Return False because entity pushes its state."""
+        return False
