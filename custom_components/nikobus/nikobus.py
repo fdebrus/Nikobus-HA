f04869f6566919f2002e5bb4cc69dfa69251b63c
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-08 16:29:16 +0100

    Update nikobus.py

--- custom_components/nikobus/nikobus.py
+++ custom_components/nikobus/nikobus.py
@@ -1,18 +1,56 @@
-import asyncio
+"""Nikobus API"""
+from __future__ import annotations
+
 import logging
-import threading
+import select
+import socket
+import ssl
+from typing import Any, Final
+
 import voluptuous as vol
 
-from enum import Enum
-from typing import Callable, Optional
-from serial import Serial, SerialException
-from serial.tools.list_ports import comports
-
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_NAME,
+    CONF_PAYLOAD,
+    CONF_PORT,
+    CONF_SSL,
+    CONF_TIMEOUT,
+    CONF_UNIT_OF_MEASUREMENT,
+    CONF_VALUE_TEMPLATE,
+    CONF_VERIFY_SSL,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import TemplateError
 import homeassistant.helpers.config_validation as cv
-from homeassistant.const import CONF_HOST, CONF_PORT
 from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.template import Template
+from homeassistant.helpers.typing import ConfigType
 
-from .const import *
+from .const import (
+    CONF_BUFFER_SIZE,
+    CONF_VALUE_ON,
+    DEFAULT_BUFFER_SIZE,
+    DEFAULT_NAME,
+    DEFAULT_SSL,
+    DEFAULT_TIMEOUT,
+    DEFAULT_VERIFY_SSL,
+)
+from .model import TcpSensorConfig
+
+TCP_PLATFORM_SCHEMA: Final[dict[vol.Marker, Any]] = {
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_PORT): cv.port,
+    vol.Required(CONF_PAYLOAD): cv.string,
+    vol.Optional(CONF_BUFFER_SIZE, default=DEFAULT_BUFFER_SIZE): cv.positive_int,
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
+    vol.Optional(CONF_UNIT_OF_MEASUREMENT): cv.string,
+    vol.Optional(CONF_VALUE_ON): cv.string,
+    vol.Optional(CONF_VALUE_TEMPLATE): cv.template,
+    vol.Optional(CONF_SSL, default=DEFAULT_SSL): cv.boolean,
+    vol.Optional(CONF_VERIFY_SSL, default=DEFAULT_VERIFY_SSL): cv.boolean,
+}
 
 __title__ = "Nikobus"
 __version__ = "0.0.1"
@@ -21,7 +59,7 @@ __license__ = "MIT"
 
 _LOGGER = logging.getLogger(__name__)
 
-class Nikobus:
+class NikobusBridge(entity):
     
     def __init__(self, hass: HomeAssistant, config: ConfigType) -> None:
         """Initialize Nikobus Connection"""
