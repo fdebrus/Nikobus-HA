1437d48af85d39e4e9261ad650a9c34821c948d5
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-08 18:40:04 +0000

    Update __init__.py

--- custom_components/nikobus/__init__.py
+++ custom_components/nikobus/__init__.py
@@ -13,15 +13,6 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
     """Set up the Nikobus component."""
     bridge = await Nikobus.create(entry.data[CONF_HOST], entry.data[CONF_PORT])
     coordinator = NikobusDataCoordinator(hass, bridge)
-    
-    coordinator.data = await hass.async_add_executor_job(bridge.get_bridge, entry.data["bridge_id"])
-    
-    hass.async_add_executor_job(bridge.subscribe, entry.data["bridge_id"], coordinator.set_updated_data)
-
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
