64f7cc3d45e14010669867fb92414f53df48d405
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-08 20:20:50 +0000

    Update config_flow.py

--- custom_components/nikobus/config_flow.py
+++ custom_components/nikobus/config_flow.py
@@ -1,28 +1,28 @@
-"""Config Flow."""
-from typing import Any, Optional
+import logging
 import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_PORT
 import homeassistant.helpers.config_validation as cv
 
-from .const import DOMAIN
-from .nikobus import Nikobus
+_LOGGER = logging.getLogger(__name__)
 
-AUTH_SCHEMA = vol.Schema(
-    {vol.Required(CONF_HOST): cv.string, vol.Required(CONF_PORT): cv.string}
-)
+DATA_SCHEMA = vol.Schema({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_PORT): cv.port,
+})
 
-class NikobusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class NikobusConfigFlow(config_entries.ConfigFlow, domain="my_component"):
     """Nikobus config flow."""
 
-    data: Optional[dict[str, Any]]
-
-    async def async_step_user(self, user_input: Optional[dict[str, Any]] = None):
+    async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
         if user_input is not None:
-            self.data = user_input
-  
+            # Check if the entered host and port are valid
+            # If they are, return the configured values
+            return self.async_create_entry(title="Nikobus - Bridge", data=user_input)
+
         return self.async_show_form(
-            step_id="user", data_schema=AUTH_SCHEMA, errors=errors
+            step_id="user",
+            data_schema=DATA_SCHEMA,
         )
