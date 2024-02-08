cea748442987597d27575a2de86d8d6eaed56287
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-08 20:11:27 +0000

    Update nikobus.py

--- custom_components/nikobus/nikobus.py
+++ custom_components/nikobus/nikobus.py
@@ -10,35 +10,26 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.dispatcher import async_dispatcher_send
 from homeassistant.helpers.entity import Entity
 
+from .const import DOMAIN
+
+DATA_LISTENER = 'nikobus_socket_listener'
+CONF_PAYLOAD_DELIMITER = '\n'
+
 _LOGGER = logging.getLogger(__name__)
 
-DOMAIN = 'tcp_socket_listener'
-DATA_LISTENER = 'tcp_socket_listener'
-CONF_PAYLOAD_DELIMITER = 'payload_delimiter'
-
-CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({
-        vol.Required(CONF_HOST): cv.string,
-        vol.Required(CONF_PORT): cv.port,
-        vol.Optional(CONF_PAYLOAD_DELIMITER, default='\n'): cv.string,
-    })
-}, extra=vol.ALLOW_EXTRA)
-
-
 def setup(hass, config):
-    """Set up the TCP socket listener."""
+    """ Set up the TCP socket listener. """
     conf = config[DOMAIN]
     listener = TcpSocketListener(hass, conf[CONF_HOST], conf[CONF_PORT], conf[CONF_PAYLOAD_DELIMITER])
     listener.start()
     hass.data[DATA_LISTENER] = listener
     return True
 
-
 class TcpSocketListener(threading.Thread):
-    """Thread to listen for TCP/IP socket events."""
+    """ Thread to listen for TCP/IP socket events. """
 
     def __init__(self, hass, host, port, delimiter):
-        """Initialize the listener."""
+        """ Initialize the listener. """
         super().__init__()
         self.hass = hass
         self.host = host
@@ -47,7 +38,7 @@ class TcpSocketListener(threading.Thread):
         self._stop_event = threading.Event()
 
     def run(self):
-        """Start the listener."""
+        """ Start the listener. """
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind((self.host, self.port))
         sock.listen(1)
@@ -74,38 +65,37 @@ class TcpSocketListener(threading.Thread):
             conn.close()
 
     def stop(self):
-        """Stop the listener."""
+        """ Stop the listener. """
         self._stop_event.set()
 
-
 class TcpSocketEventSensor(Entity):
-    """Representation of a TCP/IP socket event sensor."""
+    """ Representation of a TCP/IP socket event sensor. """
 
     def __init__(self):
-        """Initialize the sensor."""
+        """ Initialize the sensor. """
         self._state = None
 
     async def async_added_to_hass(self):
-        """Register dispatcher callback."""
+        """ Register dispatcher callback. """
         self.async_on_remove(async_dispatcher_connect(
             self.hass, DATA_LISTENER, self._update_callback))
 
     async def _update_callback(self, data):
-        """Handle event updates."""
+        """ Handle event updates. """
         self._state = data
         self.async_write_ha_state()
 
     @property
     def name(self):
-        """Return the name of the sensor."""
+        """ Return the name of the sensor. """
         return 'TCP Socket Event'
 
     @property
     def state(self):
-        """Return the state of the sensor."""
+        """ Return the state of the sensor. """
         return self._state
 
     @property
     def should_poll(self):
-        """Disable polling."""
+        """ Disable polling. """
         return False
