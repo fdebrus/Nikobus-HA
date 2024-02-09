f40ba38f16e69f74e725b8fe79698bcfa8c154fc
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-09 22:30:01 +0100

    Update const.py

--- custom_components/nikobus/const.py
+++ custom_components/nikobus/const.py
@@ -5,6 +5,8 @@ from typing import Final
 DOMAIN = "nikobus"
 MANUFACTURER = "Niko"
 
+CONF_BUFFER_SIZE: Final = "buffer_size"
+
 DEFAULT_BUFFER_SIZE: Final = 1024
 DEFAULT_NAME: Final = "HF2211"
 DEFAULT_TIMEOUT: Final = 10
