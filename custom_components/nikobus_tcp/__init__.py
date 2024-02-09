222480a9c6474d067d35e1e7264e014a9a177874
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-09 16:10:08 +0100

    Update __init__.py

--- custom_components/nikobus_tcp/__init__.py
+++ custom_components/nikobus_tcp/__init__.py
@@ -1,16 +1,37 @@
-from homeassistant import config_entries, core
-from homeassistant.components import binary_sensor, light, switch, sensor, select
-from homeassistant.const import CONF_HOST, CONF_PORT
+"""The Nikobus component."""
+from nikobus import real_time_api
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import DOMAIN
-from .nikobus import Nikobus
 
-async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
-    """Set up the Nikobus component."""
-    api = await Nikobus.create(hass, entry.data[CONF_HOST], entry.data[CONF_PORT])
+PLATFORMS = [Platform.SENSOR]
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up the sensors from a ConfigEntry."""
+
+    try:
+        api = await real_time_api(
+            entry.data[CONF_IP_ADDRESS],
+            entry.data[CONF_PORT],
+        )
+        await api.get_data()
+    except Exception as err:
+        raise ConfigEntryNotReady from err
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = api
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
 
-async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
-    """Async setup component."""
-    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
 
