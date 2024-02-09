252df7c362a5314ba51eb0cf3304427a375deae1
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-09 13:59:49 +0100

    Create config_flow.py

---
+++ custom_components/nikobus/config_flow.py
@@ -0,0 +1,28 @@
+import logging
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_PORT
+
+import homeassistant.helpers.config_validation as cv
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+DATA_SCHEMA = vol.Schema({
+    vol.Required(CONF_PORT): cv.string,
+})
+
+class NikobusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Nikobus config flow."""
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initialized by the user."""
+        if user_input is not None:
+            return self.async_create_entry(title="NikobusBridge", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=DATA_SCHEMA,
+        )
