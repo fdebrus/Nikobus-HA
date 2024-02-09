66f1debca0b6b8be8e910d9cbc95f0c3d2ddf291
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-09 11:16:28 +0100

    Update config_flow.py

--- custom_components/nikobus/config_flow.py
+++ custom_components/nikobus/config_flow.py
@@ -1,11 +1,8 @@
 import logging
 import voluptuous as vol
-
 from homeassistant import config_entries
 from homeassistant.const import CONF_HOST, CONF_PORT
-
 import homeassistant.helpers.config_validation as cv
-
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
@@ -20,9 +17,13 @@ class NikobusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle a flow initialized by the user."""
+        _LOGGER.debug("Entered async_step_user")
+
         if user_input is not None:
+            _LOGGER.debug("User input received: %s", user_input)
             return self.async_create_entry(title="NikobusBridge", data=user_input)
 
+        _LOGGER.debug("Showing form to the user")
         return self.async_show_form(
             step_id="user",
             data_schema=DATA_SCHEMA,
