0f7de458b6ccc105b04a74dbb1fc8fd4b54c232d
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-13 11:48:41 +0100

    Update nikobus.py

--- custom_components/nikobus/nikobus.py
+++ custom_components/nikobus/nikobus.py
@@ -1,114 +1,41 @@
-"""Nikobus component."""
-from __future__ import annotations
-
 import logging
-import select
-import socket
-from typing import Any, Final
+from typing import Any
 
-import voluptuous as vol
+from simple_socket.tcp_client import SimpleTCPClient
 
-from homeassistant.const import (
-    CONF_HOST,
-    CONF_PORT,
-)
-#    CONF_NAME,
-#    CONF_TIMEOUT,
-#    CONF_BUFFER_SIZE,
+_LOGGER = logging.getLogger(__name__)
 
+__title__ = "Nikobus"
+__version__ = "0.0.0"
+__author__ = "Frederic Debrus"
+__license__ = "MIT"
 
-from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import TemplateError
-import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.template import Template
-from homeassistant.helpers.typing import ConfigType
+class Nikobus:
+    """Nikobus API."""
 
-from .const import (
-    DEFAULT_BUFFER_SIZE,
-    DEFAULT_TIMEOUT,
-    DEFAULT_NAME,
-    CONF_BUFFER_SIZE,
-)
+    def __init__(self, aioTCP_session: SimpleTCPClient, hostname: str, ipport: str) -> None:
+        """Initialize Nikobus API."""
+        self.aioTCP_session = aioTCP_session
+        self._hostname = hostname
+        self._ipport = ipport
+        self._autoConnect = True
+        self.handlers = []
 
-_LOGGER: Final = logging.getLogger(__name__)
+    @classmethod
+    async def create(cls, aioTCP_session: SimpleTCPClient, hostname: str, ipport: str):
+        """Initialize Nikobus."""
+        instance = aioTCP_session(hostname, ipport)
+        return instance
 
-"""
-TCP_PLATFORM_SCHEMA: Final[dict[vol.Marker, Any]] = {
-    vol.Required(CONF_HOST): cv.string,
-    vol.Required(CONF_PORT): cv.port,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
-    vol.Optional(CONF_BUFFER_SIZE, default=DEFAULT_BUFFER_SIZE): cv.positive_int,
-}
-"""
+    async def get_data(self):
+        """Retrieve data from the Nikobus system."""
+        try:
+            data = await self.aioTCP_session.get_data()
+            _LOGGER.debug("Data received: %s", data)
+            return data
+        except Exception as e:
+            _LOGGER.error("Failed to retrieve data from Nikobus: %s", e)
+            raise
 
-class TcpEntity(Entity):
-    """Base entity class for TCP platform."""
-
-    def __init__(self, hass: HomeAssistant, config: ConfigType) -> None:
-        value_template.hass = hass
-        self._hass = hass
-        self._state: str | None = None
-        self.update()
-
-    @property
-    def name(self) -> str:
-        """Return the name of this sensor."""
-        return self._config[CONF_NAME]
-
-    def update(self, host, port) -> None:
-        """Get the latest value for this sensor."""
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.settimeout(self._config[CONF_TIMEOUT])
-            try:
-                sock.connect((host, port))
-            except OSError as err:
-                _LOGGER.error(
-                    "Unable to connect to %s on port %s: %s",
-                    host,
-                    port,
-                    err,
-                )
-                return
-
-            try:
-                sock.send(self._config[CONF_PAYLOAD].encode())
-            except OSError as err:
-                _LOGGER.error(
-                    "Unable to send payload %r to %s on port %s: %s",
-                    self._config[CONF_PAYLOAD],
-                    host,
-                    port,
-                    err,
-                )
-                return
-
-            readable, _, _ = select.select([sock], [], [], self._config[CONF_TIMEOUT])
-            if not readable:
-                _LOGGER.warning(
-                    (
-                        "Timeout (%s second(s)) waiting for a response after "
-                        "sending %r to %s on port %s"
-                    ),
-                    self._config[CONF_TIMEOUT],
-                    self._config[CONF_PAYLOAD],
-                    host,
-                    port,
-                )
-                return
-
-            value = sock.recv(self._config[CONF_BUFFER_SIZE]).decode()
-            
-        if value is not None:
-            try:
-                self._state = value
-                return
-            except TemplateError:
-                _LOGGER.error(
-                    "Unable to read value: %r",
-                    value,
-                )
-                return
-
-        self._state = value
+class UnauthorizedException(Exception):
+    """Unauthorized user exception."""
