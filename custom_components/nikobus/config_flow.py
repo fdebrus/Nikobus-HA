7a6974e04d5eef7e7cbd305bf9d15606ed1e3a79
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-13 11:47:54 +0100

    Update config_flow.py

--- custom_components/nikobus/config_flow.py
+++ custom_components/nikobus/config_flow.py
@@ -1,65 +1,42 @@
-"""Config flow for Nikobus integration."""
-from __future__ import annotations
+"""Config Flow."""
+from typing import Any, Optional
 
-import logging
-from typing import Any
-
-# from nikobus import real_time_api
-# from nikobus.discovery import DiscoveryError
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT
-from homeassistant.data_entry_flow import FlowResult
+from homeassistant.const import CONF_HOST, CONF_PORT
 import homeassistant.helpers.config_validation as cv
 
+from simple_socket.tcp_client import SimpleTCPClient
+
 from .const import DOMAIN
+from .nikobus import Nikobus, UnauthorizedException
 
-_LOGGER = logging.getLogger(__name__)
-
-DEFAULT_PORT = 9999
-
-STEP_USER_DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_IP_ADDRESS): cv.string,
-        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-    }
+STEP_USER_CONNECTIVITY = vol.Schema(
+    {vol.Required(CONF_HOST): cv.string, vol.Required(CONF_PORT): cv.port}
 )
 
-"""
-async def validate_api(data) -> str:
-    api = await update(
-        data[CONF_IP_ADDRESS], data[CONF_PORT]
-    )
-    response = await api.get_data()
-    return response.serial_number
-"""
+class NikobusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Nikobus config flow."""
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Nikobus."""
+    async def async_step_user(self, user_input: Optional[dict[str, Any]] = None):
+        """Handle a flow initialized by the user."""
+        errors = {}
+        if user_input is not None:
+            try:
+                TcpSocket = await Nikobus.create(SimpleTCPClient, user_input[CONF_HOST], user_input[CONF_PORT])
+                self.data = user_input
+                self.data['bridge'] = TcpSocket.Connect()
+                return self.async_create_entry(title="Nikobus Bridge", data=self.data)
+            except UnauthorizedException:
+                errors["base"] = "auth_error"
+        return self.async_show_form(step_id="user", data_schema=STEP_USER_CONNECTIVITY, errors=errors)
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle the initial step."""
-        errors: dict[str, Any] = {}
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
-            )
-"""
-        try:
-            await update(user_input)
-        except (ConnectionError, DiscoveryError):
-            errors["base"] = "cannot_connect"
-        except Exception:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception")
-            errors["base"] = "unknown"
-        else:
-            await self.async_set_unique_id(serial_number)
-            self._abort_if_unique_id_configured()
-            return self.async_create_entry(title=serial_number, data=user_input)
-"""
-        return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
-        )
+    async def async_create_entry(self, title: str, data: dict) -> dict:
+        """Create an entry."""
+        existing_entry = self.hass.config_entries.async_entries(DOMAIN)
+        if existing_entry:
+            self.hass.config_entries.async_update_entry(existing_entry[0], data=data)
+            await self.hass.config_entries.async_reload(existing_entry[0].entry_id)
+            return self.async_abort(reason="reauth_successful")
+        return super().async_create_entry(title=title, data=data)
