699ecf0f440105fe30eeb6148412e960ffe2b975
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-14 17:30:04 +0100

    Update __init__.py

--- custom_components/nikobus/__init__.py
+++ custom_components/nikobus/__init__.py
@@ -1,11 +1,10 @@
 """The Nikobus integration."""
 
+import asyncio
 import logging
 
 from homeassistant import config_entries, core
 from homeassistant.const import CONF_HOST, CONF_PORT
-
-from simple_socket.tcp_client import SimpleTCPClient
 
 from .const import DOMAIN
 from .nikobus import Nikobus
@@ -14,15 +13,18 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
     """Set up the Nikobus component."""
-    try:
-        api = await Nikobus.create(SimpleTCPClient, entry.data[CONF_HOST], entry.data[CONF_PORT])
-        return True
-    except Exception as e:
-        _LOGGER.error("Error setting up Nikobus component: %s", e)
-        return False
+    hostname = entry.data.get(CONF_HOST)
+    port = entry.data.get(CONF_PORT)
 
-    coordinator.data = await hass.async_add_executor_job(Nikobus.get_data)
-    
+    reader, writer = await Nikobus.connect_bridge(hostname=hostname, port=port)
+    _LOGGER.debug("Nikobus connected: %s / %s", reader, writer)
+
+    # Perform an initial check or command to verify that the connection is working (pseudo-code)
+    # await api.verify_connection()
+
+    # Store API instance for later use
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = writer
+
     return True
 
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
