05a08c549a481aa5c38f4a97c82ef11091fbf934
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-14 17:56:13 +0100

    Update __init__.py

--- custom_components/nikobus/__init__.py
+++ custom_components/nikobus/__init__.py
@@ -16,14 +16,14 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
     hostname = entry.data.get(CONF_HOST)
     port = entry.data.get(CONF_PORT)
 
-    reader, writer = await Nikobus.connect_bridge(hostname=hostname, port=port)
-    _LOGGER.debug("Nikobus connected: %s / %s", reader, writer)
+    api = await Nikobus.connect_bridge(hostname=hostname, port=port)
+    _LOGGER.debug("Nikobus connected: %s", api)
 
     # Perform an initial check or command to verify that the connection is working (pseudo-code)
     # await api.verify_connection()
 
     # Store API instance for later use
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = writer
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = api
 
     return True
 
