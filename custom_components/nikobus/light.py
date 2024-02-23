b252e7144c843665a75a33d18f39d8e0fc7f0319
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-02-23 09:10:39 +0100

    Update light.py

--- custom_components/nikobus/light.py
+++ custom_components/nikobus/light.py
@@ -1,18 +1,21 @@
+"""Nikobus Dimmer / Light entity."""
 import logging
-import json
-
 from typing import Optional
 
+# Importing required modules from Home Assistant
 from homeassistant.components.light import LightEntity, SUPPORT_BRIGHTNESS
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
+# Importing constants
 from .const import DOMAIN, BRAND
 
 _LOGGER = logging.getLogger(__name__)
 
+# Entry setup function
 async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities) -> bool:
     """Set up a config entry."""
+    # Getting data service from Home Assistant data using entry ID
     dataservice = hass.data[DOMAIN].get(entry.entry_id)
     entities = []
 
@@ -23,19 +26,22 @@ async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities) -> b
         address = dimmer_module.get("address")
         channels = dimmer_module["channels"]
         for i in range(len(channels)):
-            ch_description = channels[i]["description"]
-            entities.append(NikobusLightEntity(hass, dataservice, description, model, address, i, ch_description))
+            channel_description = channels[i]["description"]
+            # Create NikobusLightEntity instance for each channel
+            entities.append(NikobusLightEntity(hass, dataservice, description, model, address, i, channel_description))
 
+    # Add created entities to Home Assistant
     async_add_entities(entities)
 
+# NikobusLightEntity class
 class NikobusLightEntity(CoordinatorEntity, LightEntity):
     """Nikobus Light Entity."""
 
-    def __init__(self, hass:  HomeAssistant, dataservice, description, model, address, channel, ch_description, initial_state=False, brightness=255) -> None:
+    def __init__(self, hass:  HomeAssistant, dataservice, description, model, address, channel, channel_description, initial_state=False, brightness=255) -> None:
         """Initialize a Nikobus Light Entity."""
         super().__init__(dataservice)
         self._dataservice = dataservice
-        self._name = ch_description
+        self._name = channel_description
         self._state = initial_state
         self._brightness = brightness
         self._description = description
@@ -44,6 +50,7 @@ class NikobusLightEntity(CoordinatorEntity, LightEntity):
         self._channel = channel
         self._unique_id = f"{self._address}{self._channel}"
 
+    # device_info property
     @property
     def device_info(self):
         """Return the device info."""
@@ -54,45 +61,55 @@ class NikobusLightEntity(CoordinatorEntity, LightEntity):
             "model": self._model,
         }
 
+    # name property
     @property
     def name(self):
         """Return the name of the light."""
         return self._name
 
+    # brightness property
     @property
     def brightness(self):
+        """Return the brightness of the light."""
         self._brightness = self._dataservice.get_light_brightness(self._address, self._channel)
         return self._brightness
 
+    # supported_features property
     @property
     def supported_features(self):
         """Flag supported features."""
         return SUPPORT_BRIGHTNESS 
 
+    # is_on property
     @property
     def is_on(self):
+        """Return the current state of the light."""
         self._state = self._dataservice.get_light_state(self._address, self._channel)
         return self._state
 
+    # update method
     def update(self):
-        """Update the state of the cover."""
+        """Update the state of the light."""
         self._state = self._dataservice.get_output_state(self._address, self._channel)
         return self._state
 
+    # async_turn_on method
     async def async_turn_on(self, **kwargs):
-        """Turn the entity on."""
+        """Turn the light on."""
         self._brightness = kwargs.get("brightness", 255)
         await self._dataservice.turn_on_light(self._address, self._channel, self._brightness)
         self._state = True
         self.schedule_update_ha_state()
 
+    # async_turn_off method
     async def async_turn_off(self, **kwargs):
-        """Turn the entity off."""
+        """Turn the light off."""
         await self._dataservice.turn_off_light(self._address, self._channel)
         self._state = False
         self.schedule_update_ha_state()
-        
+
+    # unique_id property
     @property
     def unique_id(self):
-        """The unique id of the sensor."""
+        """Return the unique ID of the light."""
         return self._unique_id
