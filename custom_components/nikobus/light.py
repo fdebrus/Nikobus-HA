cc77630c784253351db7d4ff390eed149bcf1ebc
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-03-03 17:40:29 +0100

    Update light.py

--- custom_components/nikobus/light.py
+++ custom_components/nikobus/light.py
@@ -34,6 +34,7 @@ async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities) -> b
         )
         for dimmer_module in dataservice.api.json_config_data["dimmer_modules_addresses"]
         for i, channel in enumerate(dimmer_module["channels"], start=1)
+        if not channel["description"].startswith("not_in_use")
     ]
 
     # Add created entities to Home Assistant
