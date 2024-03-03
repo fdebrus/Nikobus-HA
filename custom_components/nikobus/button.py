603a5d06de216bc7941fe85178f001ee2bf01143
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-03-03 21:31:30 +0100

    Update button.py

--- custom_components/nikobus/button.py
+++ custom_components/nikobus/button.py
@@ -1,24 +1,33 @@
 """Nikobus Button entity."""
+
 import logging
 
-# Importing required modules from Home Assistant
+# Importing necessary classes from Home Assistant for button entities
 from homeassistant.components.button import ButtonEntity
-from homeassistant.core import HomeAssistant
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-# Importing constants
+# Importing constants used in the integration
 from .const import DOMAIN, BRAND
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities) -> bool:
-    """Set up a config entry."""
-    # Getting data service from Home Assistant data using entry ID
+    """Set up Nikobus button entities for a specific configuration entry.
+
+    This function is called by Home Assistant when setting up a configuration entry
+    for Nikobus. It initializes all button entities based on the Nikobus configuration.
+
+    Parameters:
+    - hass: Instance of HomeAssistant.
+    - entry: The configuration entry being set up.
+    - async_add_entities: Callback to add new entities to Home Assistant.
+    """
+    # Retrieve the data service associated with the configuration entry
     dataservice = hass.data[DOMAIN].get(entry.entry_id)
 
-    # Iterate over button
+    # Create a list of NikobusButtonEntity instances from the data provided by the dataservice
     entities = [
         NikobusButtonEntity(
             hass,
@@ -32,16 +41,24 @@ async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities) -> b
         for i, impacted_module in enumerate(button["impacted_module"])
     ]
 
-    # Add created entities to Home Assistant
+    # Add the entities to Home Assistant
     async_add_entities(entities)
 
 class NikobusButtonEntity(CoordinatorEntity, ButtonEntity):
-    """Representation of a Nikobus Button."""
+    """Represents a Nikobus Button in Home Assistant."""
 
-    def __init__(self, hass:  HomeAssistant, dataservice, description, address, impacted_module_address, impacted_module_group) -> None:
-        """Initialize a Nikobus Light Entity."""
+    def __init__(self, hass: HomeAssistant, dataservice, description, address, impacted_module_address, impacted_module_group) -> None:
+        """Initialize the Nikobus Button Entity.
+
+        Parameters:
+        - hass: Instance of HomeAssistant.
+        - dataservice: The dataservice associated with this entity.
+        - description: The human-readable description of the button.
+        - address: The unique address of the button.
+        - impacted_module_address: The address of the module impacted by this button.
+        - impacted_module_group: The group of the module impacted by this button.
+        """
         super().__init__(dataservice)
-        self._dataservice = dataservice
         self._description = description
         self._address = address 
         self.impacted_module_address = impacted_module_address
@@ -51,19 +68,24 @@ class NikobusButtonEntity(CoordinatorEntity, ButtonEntity):
 
     @property
     def device_info(self):
-        """Return the device info."""
+        """Return device information for this button entity."""
         return {
             "identifiers": {(DOMAIN, self._address)},
             "name": self._description,
             "manufacturer": BRAND,
             "model": "Push Button"
         }
+
     @property
     def extra_state_attributes(self) -> dict[str, str] | None:
-        """Return extra attributes."""
+        """Return extra state attributes for this button entity."""
         return {"impacted_module": f"{self.impacted_module_address}_{self.impacted_module_group}"}
 
     async def async_press(self) -> None:
-        """Handle the button press."""
+        """Handle the button press event.
+
+        This method is called when the button is pressed in the Home Assistant UI.
+        It triggers the corresponding action in the Nikobus system.
+        """
         _LOGGER.debug("Nikobus Button Pressed")
         await self._dataservice.send_button_press(self._address)
