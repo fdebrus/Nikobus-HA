6c1af07a9853fe5d1d064a38472d86e2b87f2bc7
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-03-05 15:44:05 +0100

    Update __init__.py

--- custom_components/nikobus/__init__.py
+++ custom_components/nikobus/__init__.py
@@ -20,20 +20,6 @@ PLATFORMS = [switch.DOMAIN, light.DOMAIN, cover.DOMAIN, button.DOMAIN]
 CONF_CONNECTION_STRING = "connection_string"
 
 async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
-    """Set up Nikobus from a config entry.
-
-    This function initializes the Nikobus integration in Home Assistant,
-    using the configuration entry provided by the user during the setup process.
-    It creates an instance of the Nikobus API, sets up a data coordinator for fetching updates,
-    and forwards the setup to the specified platforms (switch, light, cover, button).
-
-    Parameters:
-    - hass: The HomeAssistant instance.
-    - entry: The configuration entry created by the user.
-
-    Returns:
-    - True if setup was successful, False otherwise.
-    """
 
     # Retrieve the connection string from the configuration entry
     connection_string = entry.data.get(CONF_CONNECTION_STRING)
@@ -52,6 +38,8 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
     # Store the coordinator in Home Assistant's data dictionary under the integration's domain
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
+    api.coordinator = coordinator
+
     # Refresh the data from Nikobus for the first time
     await coordinator.async_config_entry_first_refresh()
 
@@ -59,7 +47,6 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     # Create a background task to process incoming commands from Nikobus
-    # This ensures that command processing doesn't block the setup process
     hass.loop.create_task(api.process_commands())
 
     return True
