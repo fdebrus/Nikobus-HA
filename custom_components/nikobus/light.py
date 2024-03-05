89bc7fba520b17baebf517acf5f2370c1342ff1d
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-03-05 21:32:52 +0100

    Update light.py

--- custom_components/nikobus/light.py
+++ custom_components/nikobus/light.py
@@ -12,13 +12,8 @@ from .const import DOMAIN, BRAND
 
 _LOGGER = logging.getLogger(__name__)
 
-UPDATE_SIGNAL = "update_signal"
-
 async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities) -> bool:
-    """Set up Nikobus light entities from a configuration entry.
-    
-    This function initializes light entities based on the dimmer modules configured in the Nikobus system.
-    """
+
     dataservice = hass.data[DOMAIN].get(entry.entry_id)
 
     entities = [
@@ -39,10 +34,8 @@ async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities) -> b
     async_add_entities(entities)
 
 class NikobusLightEntity(CoordinatorEntity, LightEntity):
-    """Represents a Nikobus dimmer (light) entity within Home Assistant."""
 
     def __init__(self, hass: HomeAssistant, dataservice, description, model, address, channel, channel_description, initial_state=False, brightness=255) -> None:
-        """Initialize the Nikobus Light Entity with specific parameters."""
         super().__init__(dataservice)
         self._dataservice = dataservice
         self._state = initial_state
@@ -57,7 +50,6 @@ class NikobusLightEntity(CoordinatorEntity, LightEntity):
 
     @property
     def device_info(self):
-        """Return device information for Home Assistant."""
         return {
             "identifiers": {(DOMAIN, self._address)},
             "name": self._description,
@@ -67,40 +59,33 @@ class NikobusLightEntity(CoordinatorEntity, LightEntity):
 
     @property
     def brightness(self):
-        """Return the brightness of the light."""
         return self._brightness
 
     @property
     def color_mode(self):
-        """Return the color mode of the light, supporting only brightness."""
         return "brightness"
 
     @property
     def supported_color_modes(self):
-        """Return the supported color modes, which is brightness only for this entity."""
         return {"brightness"}
 
     @property
     def is_on(self):
-        """Return the current state of the light (on/off)."""
         return self._state
 
     @callback
     def _handle_coordinator_update(self) -> None:
         self._state = bool(self._dataservice.get_light_state(self._address, self._channel))
         self._brightness = self._dataservice.get_light_brightness(self._address, self._channel)
-        _LOGGER.debug(f"LIGHT COORDINATOR UPDATE {self._state} - {self._brightness}.")
         self.async_write_ha_state()
 
     async def async_turn_on(self, **kwargs):
-        """Turn the light on with the specified brightness."""
         self._brightness = kwargs.get("brightness", 255)
         self._state = True
         await self._dataservice.turn_on_light(self._address, self._channel, self._brightness)
         self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
-        """Turn the light off."""
         self._state = False
         await self._dataservice.turn_off_light(self._address, self._channel)
         self.async_write_ha_state()
