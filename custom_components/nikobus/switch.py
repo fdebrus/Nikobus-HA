7fb9a8ef2cace2d1021270838df13f1906fe1534
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-03-05 21:35:38 +0100

    Update switch.py

--- custom_components/nikobus/switch.py
+++ custom_components/nikobus/switch.py
@@ -1,7 +1,5 @@
 """Nikobus Switch entity."""
 
-import logging
-
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
@@ -9,15 +7,8 @@ from homeassistant.helpers.dispatcher import async_dispatcher_connect, async_dis
 
 from .const import DOMAIN, BRAND
 
-_LOGGER = logging.getLogger(__name__)
-
-UPDATE_SIGNAL = "update_signal"
-
 async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities) -> bool:
-    """Set up Nikobus switch entities from a configuration entry.
-    
-    This function initializes switch entities based on the switch modules configured in the Nikobus system.
-    """
+
     dataservice = hass.data[DOMAIN].get(entry.entry_id)
 
     entities = [
@@ -38,10 +29,8 @@ async def async_setup_entry(hass: HomeAssistant, entry, async_add_entities) -> b
     async_add_entities(entities)
 
 class NikobusSwitchEntity(CoordinatorEntity, SwitchEntity):
-    """Represents a Nikobus switch entity within Home Assistant."""
 
     def __init__(self, hass: HomeAssistant, dataservice, description, model, address, channel, channel_description, initial_state=False) -> None:
-        """Initialize the Nikobus Switch Entity with specific parameters."""
         super().__init__(dataservice)
         self._dataservice = dataservice
         self._state = initial_state
@@ -55,7 +44,6 @@ class NikobusSwitchEntity(CoordinatorEntity, SwitchEntity):
 
     @property
     def device_info(self):
-        """Return device information for Home Assistant."""
         return {
             "identifiers": {(DOMAIN, self._address)},
             "name": self._description,
@@ -65,24 +53,19 @@ class NikobusSwitchEntity(CoordinatorEntity, SwitchEntity):
 
     @property
     def is_on(self):
-        """Return the current state of the switch (on/off)."""
         return self._state
 
     @callback
     def _handle_coordinator_update(self) -> None:
-        """Handle updated data from the coordinator."""
         self._state = bool(self._dataservice.get_switch_state(self._address, self._channel))
-        _LOGGER.debug(f"SWITCH COORDINATOR UPDATE {self._state}.")
         self.async_write_ha_state()
 
     async def async_turn_on(self):
-        """Turn the switch on."""
         self._state = True
         await self._dataservice.turn_on_switch(self._address, self._channel)
         self.async_write_ha_state()
 
     async def async_turn_off(self):
-        """Turn the switch off."""
         self._state = False
         await self._dataservice.turn_off_switch(self._address, self._channel)
         self.async_write_ha_state()
