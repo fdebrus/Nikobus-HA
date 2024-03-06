3ff0036cf8c1d6569ca30a4d8c009b6f0106d54d
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-03-06 17:52:04 +0100

    Update __init__.py

--- custom_components/nikobus/__init__.py
+++ custom_components/nikobus/__init__.py
@@ -6,6 +6,7 @@ import asyncio
 from homeassistant import config_entries, core
 from homeassistant.core import HomeAssistant
 from homeassistant.components import switch, light, cover, button
+from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import DOMAIN
 from .nikobus import Nikobus
@@ -13,35 +14,20 @@ from .coordinator import NikobusDataCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-# Define the platforms that the Nikobus integration will set up
 PLATFORMS = [switch.DOMAIN, light.DOMAIN, cover.DOMAIN, button.DOMAIN]
-
-# Configuration key for the connection string
-CONF_CONNECTION_STRING = "connection_string"
 
 async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
 
-    # Retrieve the connection string from the configuration entry
-    connection_string = entry.data.get(CONF_CONNECTION_STRING)
+    coordinator = NikobusDataCoordinator(hass, entry)
 
-    # Initialize the Nikobus API with the provided connection string
-    api = await Nikobus.create(hass, connection_string)
+    success = await coordinator.connect()
 
-    # Create a data coordinator for the Nikobus system
-    coordinator = NikobusDataCoordinator(hass, api)
-    
-    # Store the coordinator in Home Assistant's data dictionary under the integration's domain
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    api.coordinator = coordinator
-
-    # Refresh the data from Nikobus for the first time
     await coordinator.async_config_entry_first_refresh()
 
-    # Forward the setup process to each platform defined in PLATFORMS
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    # Create a background task to process incoming commands from Nikobus
-    hass.loop.create_task(api.process_commands())
+    hass.loop.create_task(coordinator.api.process_commands())
 
     return True
