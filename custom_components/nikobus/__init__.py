e0186f48283923e523635c400042d5d8a5af9a86
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-03-07 21:40:13 +0100

    Update __init__.py

--- custom_components/nikobus/__init__.py
+++ custom_components/nikobus/__init__.py
@@ -21,10 +21,12 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
     coordinator = NikobusDataCoordinator(hass, entry)
 
     success = await coordinator.connect()
-
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    # if not success:
+    #    raise ConfigEntryNotReady
 
     await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
