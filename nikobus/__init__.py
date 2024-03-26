a2cfd08723248d304b6d35276f05c65d746d467b
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-03-26 21:32:19 +0100

    Update __init__.py

--- nikobus/__init__.py
+++ nikobus/__init__.py
@@ -5,7 +5,7 @@ import asyncio
 
 from homeassistant import config_entries, core
 from homeassistant.core import HomeAssistant
-from homeassistant.components import switch, light, cover, button
+from homeassistant.components import switch, light, cover, binary_sensor, button
 from homeassistant.exceptions import ConfigEntryNotReady
 
 from .const import DOMAIN
@@ -14,7 +14,7 @@ from .coordinator import NikobusDataCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = [switch.DOMAIN, light.DOMAIN, cover.DOMAIN, button.DOMAIN]
+PLATFORMS = [switch.DOMAIN, light.DOMAIN, cover.DOMAIN, binary_sensor.DOMAIN, button.DOMAIN]
 
 async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
 
@@ -22,13 +22,18 @@ async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.Conf
 
     await coordinator.connect()
 
-    hass.loop.create_task(coordinator.api.listen_for_events())
-    hass.loop.create_task(coordinator.api.command_handler())
+    await coordinator.api.listen_for_events()
+
+    await coordinator.api.command_handler()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
+    await coordinator.async_config_entry_first_refresh()
+    
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    await coordinator.async_config_entry_first_refresh()
-
     return True
+
+async def on_hass_stop(event):
+    """Close connection when hass stops."""
+    coordinator.api.close()
