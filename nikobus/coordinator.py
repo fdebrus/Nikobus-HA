f6b412b748b65786e98465783df36f587f8a67c5
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-03-28 21:42:51 +0100

    Update coordinator.py

--- nikobus/coordinator.py
+++ nikobus/coordinator.py
@@ -1,19 +1,18 @@
 """Coordinator for Nikobus."""
-from typing import Any
-from datetime import timedelta
 
 import logging
-from .nikobus import Nikobus
+from datetime import timedelta
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-_LOGGER = logging.getLogger(__name__)
+from .nikobus import Nikobus, NikobusConnectionError, NikobusDataError
 
+_LOGGER = logging.getLogger(__name__)
 CONF_CONNECTION_STRING = "connection_string"
 
 class NikobusDataCoordinator(DataUpdateCoordinator):
+    """Coordinator for asynchronous management of Nikobus updates."""
 
     def __init__(self, hass: HomeAssistant, entry) -> None:
         self.hass = hass
@@ -29,20 +28,31 @@ class NikobusDataCoordinator(DataUpdateCoordinator):
         )
 
     async def connect(self):
-        self.api = await Nikobus.create(self.hass, self.connection_string, self.async_event_handler)
+        """Connect to the Nikobus system."""
+        try:
+            self.api = await Nikobus.create(self.hass, self.connection_string, self.async_event_handler)
+            await self.api.listen_for_events()
+            await self.api.command_handler()
+        except NikobusConnectionError as e:
+            _LOGGER.error("Failed to connect to Nikobus: %s", e)
+            raise NikobusConnectError from e        
 
     async def async_update_data(self):
+        """Fetch the latest data from Nikobus."""
         try:
-            _LOGGER.debug("calling REFRESH")
-            result = await self.api.refresh_nikobus_data()
-            return result
-        except Exception as e:
+            _LOGGER.debug("Refreshing Nikobus data")
+            return await self.api.refresh_nikobus_data()
+        except NikobusDataError as e:
             _LOGGER.error("Error fetching Nikobus data: %s", e)
             raise UpdateFailed(f"Error fetching data: {e}")
 
     async def async_event_handler(self, event, data):
+        """Handle events from Nikobus."""
         if "ha_button_pressed" in event:
             await self.api.nikobus_command_handler.queue_command(f'#N{data}\r#E1')
         elif "nikobus_button_pressed" in event:
             self.hass.bus.async_fire('nikobus_button_pressed', {'address': data})
-        self.async_update_listeners()
+        self.async_set_updated_data(self.data)
+
+class NikobusConnectError(Exception):
+    pass
