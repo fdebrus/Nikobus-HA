1f41ab1a80671d7fe2c6c9a539849680e55aa596
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-03-28 21:40:02 +0100

    Update nkbcommand.py

--- nikobus/nkbcommand.py
+++ nikobus/nkbcommand.py
@@ -1,18 +1,18 @@
+"""Command Processing for Nikobus."""
+
 import asyncio
 import logging
 
-from .nkbprotocol import (
-    make_pc_link_command, 
-    calculate_group_number
-)
+from .nkbprotocol import make_pc_link_command, calculate_group_number
 
 _LOGGER = logging.getLogger(__name__)
 
 __version__ = '0.1'
 
-# version-access:
-def get_version():
-    return __version__
+COMMAND_EXECUTION_DELAY = 0.3  # Delay between command executions in seconds
+COMMAND_ACK_WAIT_TIMEOUT = 15  # Timeout for waiting for command ACK in seconds
+COMMAND_ANSWER_WAIT_TIMEOUT = 5  # Timeout for waiting for command answer in each loop
+MAX_ATTEMPTS = 3  # Maximum attempts for sending commands and waiting for an answer
 
 class NikobusCommandHandler:
     def __init__(self, nikobus_connection, nikobus_listener, nikobus_module_states):
@@ -22,107 +22,106 @@ class NikobusCommandHandler:
         self._command_queue = asyncio.Queue()
 
     async def get_output_state(self, address: str, group: int) -> str:
-        """Retrieve the current state of an output based on its address and group."""
-        _LOGGER.debug(f'get_output_state_nikobus() - Address: {address}, Group: {group}')
+        _LOGGER.debug(f'Getting output state - Address: {address}, Group: {group}')
         command_code = 0x12 if int(group) == 1 else 0x17
         command = make_pc_link_command(command_code, address)
         return await self.send_command_get_answer(command, address)
 
     async def set_output_state(self, address: str, channel: int, value: int) -> None:
-        """Set the state of an output based on its address, channel, and value."""
-        _LOGGER.debug(f'set_output_state_nikobus() - Address: {address}, Channel: {channel}, Value: {value}')
+        _LOGGER.debug(f'Setting output state - Address: {address}, Channel: {channel}, Value: {value}')
         group = calculate_group_number(channel)
         command_code = 0x15 if int(group) == 1 else 0x16
-        if int(group) == 1:
-            values = self.nikobus_module_states[address][:6] + bytearray([0xFF])
-        elif int(group) == 2:
-            values = self.nikobus_module_states[address][6:12] + bytearray([0xFF])
+        values = self._prepare_values_for_command(address, group)
         command = make_pc_link_command(command_code, address, values)
         await self.queue_command(command)
 
     async def queue_command(self, command: str) -> None:
-        """Queue a command for execution."""
-        _LOGGER.debug(f'Queueing command for execution: {command}')
+        _LOGGER.debug(f'Queueing command: {command}')
         await self._command_queue.put(command)
 
     async def process_commands(self) -> None:
-        """Continuously process commands from the command queue."""
-        _LOGGER.info(f'Nikobus Command Processing Queue started')
+        _LOGGER.info('Starting command processing queue')
         while True:
             command = await self._command_queue.get()
-            _LOGGER.debug(f'Executing command from queue: {command}')
-            try:
-                await self.send_command(command)
-                _LOGGER.debug(f'Command executed successfully: {command}')
-            except Exception as e:
-                _LOGGER.error(f"Failed to execute command '{command}': {e}")
-            finally:
-                self._command_queue.task_done()
-            await asyncio.sleep(0.3)
+            _LOGGER.debug(f'Processing command: {command}')
+            await self._execute_command(command)
+            await asyncio.sleep(COMMAND_EXECUTION_DELAY)
 
     async def send_command(self, command):
-        _LOGGER.debug('Entering send_command()')
-        _LOGGER.debug(f'Command to send: {command}')
-    
+        _LOGGER.debug(f'Sending command: {command}')
         try:
             await self.nikobus_connection.send(command)
             _LOGGER.debug('Command sent successfully')
-            return True
         except Exception as e:
             _LOGGER.error(f'Error sending command: {e}')
-            return False
 
-    async def send_command_get_answer(self, command: str, address: str, max_attempts: int = 3) -> str | None:
-        _LOGGER.debug('Entering send_command_get_answer()')
-        _LOGGER.debug(f'Command: {command}, Address: {address}')
-        # Define the expected acknowledgment and answer signals based on the command and address.
-        _wait_command_ack = '$05' + command[3:5]
-        _wait_command_answer = '$1C' + address[2:] + address[:2]
+    async def send_command_get_answer(self, command: str, address: str) -> str | None:
+        _LOGGER.debug(f'Sending command and waiting for answer: {command}: {address}')
+        _wait_command_ack, _wait_command_answer = self._prepare_ack_and_answer_signals(command, address)
+        return await self._wait_for_ack_and_answer(command, _wait_command_ack, _wait_command_answer)
+
+    async def _execute_command(self, command: str):
+        try:
+            await self.send_command(command)
+            _LOGGER.debug(f'Command executed: {command}')
+        except Exception as e:
+            _LOGGER.error(f'Failed to execute command "{command}": {e}')
+
+    def _prepare_values_for_command(self, address: str, group: int) -> bytearray:
+        if int(group) == 1:
+            return self.nikobus_module_states[address][:6] + bytearray([0xFF])
+        elif int(group) == 2:
+            return self.nikobus_module_states[address][6:12] + bytearray([0xFF])
+
+    def _prepare_ack_and_answer_signals(self, command: str, address: str) -> tuple:
+        ack_signal = f'$05{command[3:5]}'
+        answer_signal = f'$1C{address[2:]}{address[:2]}'
+        return ack_signal, answer_signal
+
+    async def _wait_for_ack_and_answer(self, command:str, _wait_command_ack: str, _wait_command_answer: str) -> str | None:
         ack_received = False
         answer_received = False
         state = None
 
-        for attempt in range(max_attempts):
-            _LOGGER.debug(f'Attempt {attempt + 1} of {max_attempts}')
-
-            _LOGGER.debug(f'Sent command {command} waiting for ACK: {_wait_command_ack} and ANSWER: {_wait_command_answer}')
+        for attempt in range(MAX_ATTEMPTS):
+            _LOGGER.debug(f'Attempt {attempt + 1} of {MAX_ATTEMPTS}')
             await self.nikobus_connection.send(command)
 
-            end_time = asyncio.get_event_loop().time() + 15  # Set a 15-second timeout
+            end_time = asyncio.get_event_loop().time() + COMMAND_ACK_WAIT_TIMEOUT
 
             while asyncio.get_event_loop().time() < end_time:
                 try:
-                    timeout = end_time - asyncio.get_event_loop().time()  # Calculate remaining time for dynamic timeout
-                    message = await asyncio.wait_for(self.nikobus_listener.response_queue.get(), timeout=5)
+                    timeout = end_time - asyncio.get_event_loop().time()
+                    message = await asyncio.wait_for(self.nikobus_listener.response_queue.get(), timeout=COMMAND_ANSWER_WAIT_TIMEOUT)
                     _LOGGER.debug(f'Message received: {message}')
-                
-                    # Check for ACK and answer in the received message.
+
                     if _wait_command_ack in message and not ack_received:
                         _LOGGER.debug('ACK received.')
                         ack_received = True
 
                     if _wait_command_answer in message and not answer_received:
                         _LOGGER.debug('Answer received.')
-                        # Extract the state information from the message based on the expected format.
-                        state = message[message.find(_wait_command_answer) + len(_wait_command_answer) + 2:][:12]
+                        state = self._parse_state_from_message(message, _wait_command_answer)
                         answer_received = True
 
                     if ack_received and answer_received:
-                        break  # Break out of the loop if both ACK and answer have been received
+                        break
                 
                 except asyncio.TimeoutError:
                     _LOGGER.debug('Timeout waiting for ACK/Answer.')
-                    break  # Exit the while loop and potentially retry if the timeout is reached
+                    break
 
             if ack_received and answer_received:
                 _LOGGER.debug('Both ACK and Answer received successfully.')
-                break  # Exit the for loop if both ACK and answer have been received
+                return state
 
-        # Log the outcome of receiving the expected responses.
         if not ack_received:
-            _LOGGER.debug('ACK not received within timeout period after maximum attempts.')
+            _LOGGER.debug('ACK not received within timeout period.')
         if not answer_received:
-            _LOGGER.debug('Answer not received within timeout period after maximum attempts.')
+            _LOGGER.debug('Answer not received within timeout period.')
 
         return state
 
+    def _parse_state_from_message(self, message: str, answer_signal: str) -> str:
+        state_index = message.find(answer_signal) + len(answer_signal) + 2
+        return message[state_index:state_index+12]
