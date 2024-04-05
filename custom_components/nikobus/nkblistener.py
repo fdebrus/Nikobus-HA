fe1565ad798c6ea4b1ce94884d6e27aa875a5a48
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-04-05 16:04:42 +0200

    Update nkblistener.py

--- custom_components/nikobus/nkblistener.py
+++ custom_components/nikobus/nkblistener.py
@@ -1,74 +1,96 @@
-"""Nikobus Switch entity."""
+"""Listener for Nikobus."""
 
 import logging
-
-from homeassistant.components.switch import SwitchEntity
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-
-from .const import DOMAIN, BRAND
+import asyncio
+import time
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass, entry, async_add_entities) -> bool:
+__version__ = '0.1'
 
-    dataservice = hass.data[DOMAIN].get(entry.entry_id)
+class NikobusEventListener:
+    def __init__(self, hass, nikobus_connection, button_discovery_callback):
+        self._hass = hass
+        self._listener_task = None
+        self._running = False
+        self.nikobus_connection = nikobus_connection
+        self.response_queue = asyncio.Queue()
+        self._button_discovery_callback = button_discovery_callback
+        self._last_nikobus_command_received_timestamp = 0
+        self._continuous_press_detected = False
 
-    entities = [
-        NikobusSwitchEntity(
-            hass,
-            dataservice,
-            switch_module_data.get("description"),
-            switch_module_data.get("model"),
-            address,
-            i,
-            channel["description"],
-        )
-        for address, switch_module_data in dataservice.api.dict_module_data['switch_module'].items()
-        for i, channel in enumerate(switch_module_data["channels"], start=1) 
-        if not channel["description"].startswith("not_in_use")
-    ]
+    async def start(self):
+        self._running = True
+        self._listener_task = self._hass.loop.create_task(self.listen_for_events())
 
-    async_add_entities(entities)
+    async def stop(self):
+        self._running = False
+        if self._listener_task:
+            self._listener_task.cancel()
+            try:
+                await self._listener_task
+            except asyncio.CancelledError:
+                _LOGGER.info("Nikobus event listener has been stopped.")
+            self._listener_task = None
 
-class NikobusSwitchEntity(CoordinatorEntity, SwitchEntity):
+    async def listen_for_events(self) -> None:
+        """Continuously listen for and handle events from the Nikobus system."""
+        _LOGGER.info("Nikobus Event Listener started")
+        while self._running:
+            try:
+                data = await asyncio.wait_for(self.nikobus_connection.read(), timeout=10)
+                if not data:
+                    _LOGGER.warning("Nikobus connection closed")
+                    break
+                message = data.decode('utf-8').strip()
+                _LOGGER.debug(f"Listener - Receiving message: {message}")
+                self._hass.async_create_task(self.handle_message(message))
+            except asyncio.TimeoutError:
+                _LOGGER.debug("Listener - Read operation timed out. Waiting for next data...")
+            except asyncio.CancelledError:
+                _LOGGER.info("Event listener was cancelled.")
+                break
+            except Exception as e:
+                _LOGGER.error(f"Error in event listener: {e}", exc_info=True)
+                break
 
-    def __init__(self, hass: HomeAssistant, dataservice, description, model, address, channel, channel_description) -> None:
-        super().__init__(dataservice)
-        self._dataservice = dataservice
-        self._state = None
-        self._description = description
-        self._model = model
-        self._address = address
-        self._channel = channel
+    async def handle_message(self, message: str) -> None:
+        """Handle incoming messages from the Nikobus system."""
+        _LOGGER.debug(f"Handler got message: {message}")
 
-        self._attr_name = channel_description
-        self._attr_unique_id = f"{DOMAIN}_{self._address}_{self._channel}"
+        _button_command_prefix = '#N'
+        _ignore_answer = '$0E'
 
-    @property
-    def device_info(self):
-        return {
-            "identifiers": {(DOMAIN, self._address)},
-            "name": self._description,
-            "manufacturer": BRAND,
-            "model": self._model,
-        }
+        if message.startswith(_button_command_prefix):
+            address = message[2:8]
+            _LOGGER.debug(f"Handling button press for address: {address}")
 
-    @property
-    def is_on(self):
-        return self._state
+            self._hass.bus.async_fire('nikobus_button_pressed', {'address': address})
 
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        self._state = bool(self._dataservice.api.get_switch_state(self._address, self._channel))
-        self.async_write_ha_state()
+            current_time = time.monotonic()
 
-    async def async_turn_on(self):
-        self._state = True
-        await self._dataservice.api.turn_on_switch(self._address, self._channel)
-        self.async_write_ha_state()
+            # Calculate the time difference since the last command
+            time_diff = (current_time - self._last_nikobus_command_received_timestamp) * 1000
 
-    async def async_turn_off(self):
-        self._state = False
-        await self._dataservice.api.turn_off_switch(self._address, self._channel)
-        self.async_write_ha_state()
+            if time_diff > 150:
+                # If more than 150ms have passed, treat as a new command or end of a continuous press
+                self._last_nikobus_command_received_timestamp = current_time
+                if self._continuous_press_detected:
+                    self._continuous_press_detected = False
+                    _LOGGER.debug("End of Continuous Press Detected")
+                else:
+                    _LOGGER.debug("Single Press Detected")
+                    
+                await self._button_discovery_callback(address)
+            elif time_diff < 100:
+                if not self._continuous_press_detected:
+                    self._continuous_press_detected = True
+                    _LOGGER.debug("Continuous Press Detected - Skipping Processing")
+                else:
+                    _LOGGER.debug("Continuous Press Ongoing - Skipping Processing")
+            else:
+                _LOGGER.debug("Command Ignored - Too Close to Previous Command")
+
+        elif not message.startswith(_ignore_answer):
+            _LOGGER.debug(f"Adding message to response queue: {message}")
+            await self.response_queue.put(message)
