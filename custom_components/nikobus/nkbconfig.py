f2736c8e03daad11bed7193e811dc57b7b0a8e35
fdebrus <33791533+fdebrus@users.noreply.github.com>
2024-11-19 21:08:56 +0100

    Update nkbconfig.py

--- custom_components/nikobus/nkbconfig.py
+++ custom_components/nikobus/nkbconfig.py
@@ -68,9 +68,9 @@ class NikobusConfig:
             _LOGGER.info(
                 f"Button configuration file not found: {file_path}. A new file will be created upon discovering the first button."
             )
-        else:
+        elif data_type != "scene"
             raise HomeAssistantError(
-                f"{data_type.capitalize()} configuration file not found: {file_path}"
+                f"{data_type.capitalize()} mandatory configuration file not found: {file_path}"
             )
 
     async def write_json_data(self, file_name: str, data_type: str, data: dict) -> None:
