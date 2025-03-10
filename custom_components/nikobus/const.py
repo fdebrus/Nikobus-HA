1319a9536e42ed591ffbf57d133776dec432fbde
fdebrus <33791533+fdebrus@users.noreply.github.com>
2025-03-10 15:54:19 +0100

    Add files via upload

--- custom_components/nikobus/const.py
+++ custom_components/nikobus/const.py
@@ -37,10 +37,10 @@ HANDSHAKE_TIMEOUT: Final[int] = 60  # Timeout for handshake in seconds
 # Buttons
 # =============================================================================
 REFRESH_DELAY: Final[float] = 0.5  # Delay before retrieving status after button press
-DIMMER_DELAY: Final[int] = 1       # Delay before retrieving dimmer status
-SHORT_PRESS: Final[int] = 1        # Short press duration in seconds
-MEDIUM_PRESS: Final[int] = 2       # Medium press duration in seconds
-LONG_PRESS: Final[int] = 3         # Long press duration in seconds
+DIMMER_DELAY: Final[int] = 1  # Delay before retrieving dimmer status
+SHORT_PRESS: Final[int] = 1  # Short press duration in seconds
+MEDIUM_PRESS: Final[int] = 2  # Medium press duration in seconds
+LONG_PRESS: Final[int] = 3  # Long press duration in seconds
 
 # =============================================================================
 # Covers
@@ -62,33 +62,140 @@ DEVICE_INVENTORY: Final[tuple[str, str]] = ("$0510$2E", "$0522$1E")
 # =============================================================================
 # Command Execution
 # =============================================================================
-COMMAND_EXECUTION_DELAY: Final[float] = 0.7    # Delay between command executions
-COMMAND_ACK_WAIT_TIMEOUT: Final[int] = 15      # Timeout for command ACK
-COMMAND_ANSWER_WAIT_TIMEOUT: Final[int] = 5    # Timeout for each loop waiting for an answer
-MAX_ATTEMPTS: Final[int] = 3                   # Maximum retry attempts
+COMMAND_EXECUTION_DELAY: Final[float] = 0.7  # Delay between command executions
+COMMAND_ACK_WAIT_TIMEOUT: Final[int] = 15  # Timeout for command ACK
+COMMAND_ANSWER_WAIT_TIMEOUT: Final[int] = (
+    5  # Timeout for each loop waiting for an answer
+)
+MAX_ATTEMPTS: Final[int] = 3  # Maximum retry attempts
 
 # =============================================================================
 # Discovery
 # =============================================================================
 DEVICE_TYPES = {
-    "01": {"Category": "Module", "Model": "05-000-02", "Channels": 12, "Name": "Switch Module"},
-    "02": {"Category": "Module", "Model": "05-001-02", "Channels": 6, "Name": "Roller Shutter Module"},
-    "03": {"Category": "Module", "Model": "05-007-02", "Channels": 12, "Name": "Dimmer Module"},
-    "04": {"Category": "Button", "Model": "05-342", "Channels": 2, "Name": "Button with 2 Operation Points"},
-    "06": {"Category": "Button", "Model": "05-346", "Channels": 4, "Name": "Button with 4 Operation Points"},
-    "08": {"Category": "Module", "Model": "05-201", "Name": "PC Logic"},
-    "09": {"Category": "Module", "Model": "05-002-02", "Channels": 4, "Name": "Compact Switch Module"},
-    "0A": {"Category": "Module", "Model": "05-200", "Name": "PC Link"},
-    "0C": {"Category": "Button", "Model": "05-348", "Channels": 4, "Name": "IR Button with 4 Operation Points"},
-    "12": {"Category": "Button", "Model": "05-349", "Channels": 8, "Name": "Button with 8 Operation Points"},
-    "1F": {"Category": "Button", "Model": "05-311", "Channels": 2, "Name": "RF Transmitter with 2 Operation Points"},
-    "23": {"Category": "Button", "Model": "05-312", "Channels": 4, "Name": "RF Transmitter with 4 Operation Points"},
-    "25": {"Category": "Button", "Model": "05-311", "Channels": 1, "Name": "Portable RF Transmitter with 1 Operation Point"},
-    "31": {"Category": "Module", "Model": "05-002-02", "Channels": 4, "Name": "Compact Switch Module"},
-    "3F": {"Category": "Button", "Model": "05-344", "Channels": 2, "Name": "Feedback Button with 2 Operation Points"},
-    "40": {"Category": "Button", "Model": "05-347", "Channels": 4, "Name": "Feedback Button with 4 Operation Points"},
-    "42": {"Category": "Module", "Model": "05-207", "Name": "Feedback Module"},
-    "44": {"Category": "Button", "Model": "05-058", "Channels": 8, "Name": "Switch Interface"},
+    "01": {
+        "Category": "Module",
+        "Model": "05-000-02",
+        "Channels": 12,
+        "Name": "Switch Module",
+    },
+    "02": {
+        "Category": "Module",
+        "Model": "05-001-02",
+        "Channels": 6,
+        "Name": "Roller Shutter Module",
+    },
+    "03": {
+        "Category": "Module",
+        "Model": "05-007-02",
+        "Channels": 12,
+        "Name": "Dimmer Module",
+    },
+    "04": {
+        "Category": "Button",
+        "Model": "05-342",
+        "Channels": 2,
+        "Name": "Button with 2 Operation Points",
+    },
+    "06": {
+        "Category": "Button",
+        "Model": "05-346",
+        "Channels": 4,
+        "Name": "Button with 4 Operation Points",
+    },
+    "08": {
+        "Category": "Module", 
+        "Model": "05-201", 
+        "Name": "PC Logic"
+    },
+    "09": {
+        "Category": "Module",
+        "Model": "05-002-02",
+        "Channels": 4,
+        "Name": "Compact Switch Module",
+    },
+    "0A": {
+        "Category": "Module", 
+        "Model": "05-200", 
+        "Name": "PC Link"
+    },
+    "0C": {
+        "Category": "Button",
+        "Model": "05-348",
+        "Channels": 4,
+        "Name": "IR Button with 4 Operation Points",
+    },
+    "12": {
+        "Category": "Button",
+        "Model": "05-349",
+        "Channels": 8,
+        "Name": "Button with 8 Operation Points",
+    },
+    "1F": {
+        "Category": "Button",
+        "Model": "05-311",
+        "Channels": 2,
+        "Name": "RF Transmitter with 2 Operation Points",
+    },
+    "23": {
+        "Category": "Button",
+        "Model": "05-312",
+        "Channels": 4,
+        "Name": "RF Transmitter with 4 Operation Points",
+    },
+    "25": {
+        "Category": "Button",
+        "Model": "05-311",
+        "Channels": 1,
+        "Name": "Portable RF Transmitter with 1 Operation Point",
+    },
+    "28": {
+        "Category": "Button",
+        "Model": "05-7X5",
+        "Channels": 2,
+        "Name": "Motion Detector",
+    },
+    "31": {
+        "Category": "Module",
+        "Model": "05-002-02",
+        "Channels": 4,
+        "Name": "Compact Switch Module",
+    },
+    "3F": {
+        "Category": "Button",
+        "Model": "05-060-02",
+        "Channels": 2,
+        "Name": "Feedback Button with 2 Operation Points",
+    },
+    "40": {
+        "Category": "Button",
+        "Model": "05-064-02",
+        "Channels": 4,
+        "Name": "Feedback Button with 4 Operation Points",
+    },
+    "41": {
+        "Category": "Button",
+        "Model": "05-078-02",
+        "Channels": 8,
+        "Name": "Feedback Button with 8 Operation Points",
+    },
+    "42": {
+        "Category": "Module", 
+        "Model": "05-207", 
+        "Name": "Feedback Module"
+    },
+    "43": {
+        "Category": "Button",
+        "Model": "05-058",
+        "Channels": 4,
+        "Name": "Universal interface",
+    },
+    "44": {
+        "Category": "Button",
+        "Model": "05-058",
+        "Channels": 8,
+        "Name": "Switch Interface",
+    },
 }
 
 CHANNEL_MAPPING = {
