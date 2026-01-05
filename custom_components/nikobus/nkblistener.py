ca91cf0ab5a0993568bb4bd38b7a03d404ab6805
fdebrus <33791533+fdebrus@users.noreply.github.com>
2026-01-05 11:06:20 +0100

    Refine PC Link discovery inventory flow

--- custom_components/nikobus/nkblistener.py
+++ custom_components/nikobus/nkblistener.py
@@ -159,13 +159,7 @@ class NikobusEventListener:
         if DEVICE_ADDRESS_INVENTORY in message:
             _LOGGER.debug("Device address inventory: %s", message)
             if discovery_running:
-                self.nikobus_discovery._schedule_inventory_timeout()
-                module_address = self.nikobus_discovery.normalize_module_address(
-                    message[3:7],
-                    source="device_address_inventory",
-                    reverse_bus_order=True,
-                )
-                await self.nikobus_discovery.query_module_inventory(module_address)
+                self.nikobus_discovery.handle_device_address_inventory(message)
             else:
                 await self.nikobus_discovery.process_mode_button_press(message)
             return
